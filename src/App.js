1dd407fb157db6137c6d87513caba105ccfb9e0d
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2024-05-22 19:24:22 -0400

    CalcAvg page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,10 @@
-import { useState, useMemo, useEffect } from "react";
-import Form from "./components/Form";
-import Results from "./components/Results.js";
-import {
-  calcStockTransactions,
-  calcAverages,
-  calcPositiveSharePrice,
-  calcNegativeSharePrice,
-} from "./utils.js";
 import "./App.css";
+import CalcAvg from "./pages/CalcAvg";
 
 function App() {
-  const [stockEntries, setStockEntries] = useState([]);
-  const [results, setResults] = useState(null);
-
-  const averages = useMemo(
-    () => calcAverages(calcStockTransactions(stockEntries)),
-    [stockEntries]
-  );
-
-  useEffect(() => {
-    if (averages.totalShares > 0) {
-      const percentages = [0.03, 0.07, 0.13, 0.21];
-      const newResults = percentages.reduce((acc, percentage) => {
-        const positiveSharePrice = calcPositiveSharePrice(averages, percentage);
-        const negativeSharePrice = calcNegativeSharePrice(averages, percentage);
-
-        acc[Math.round(percentage * 100)] = {
-          sharePrice: {
-            positive: positiveSharePrice,
-            negative: negativeSharePrice,
-          },
-          costBasis: {
-            positive: averages.totalShares * positiveSharePrice,
-            negative: averages.totalShares * negativeSharePrice,
-          },
-        };
-
-        return acc;
-      }, {});
-
-      setResults(newResults);
-    }
-  }, [averages]);
-
-  function formSubmitHandler(formFields) {
-    setStockEntries(formFields);
-  }
-
   return (
     <div className="container">
-      <Form formSubmitHandler={formSubmitHandler} />
-      {results !== null && <Results averages={averages} results={results} />}
+      <CalcAvg />
     </div>
   );
 }
