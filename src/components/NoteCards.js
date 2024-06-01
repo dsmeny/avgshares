8e5e24c83cc528665686faaa75f70df350ac4ad9
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2024-06-01 19:49:35 -0400

    editing a label

--- src/components/NoteCards.js
+++ src/components/NoteCards.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import { FiEdit2 } from "react-icons/fi";
 
 const NoteCards = ({ db }) => {
   return (
@@ -11,12 +12,51 @@ const NoteCards = ({ db }) => {
 };
 
 function NoteCard({ messages }) {
+  const [label, setLabel] = useState("");
+  const [isReadOnly, setIsReadOnly] = useState(true);
   const { header, body } = messages;
+
+  const inputRef = useRef();
+
+  function editLabel() {
+    setIsReadOnly((prev) => !prev);
+  }
+
+  function changeHandler(e) {
+    const value = e.target.value;
+    setLabel(value);
+  }
+
+  function eventHandler(e) {
+    if (e.keyCode === 13) {
+      setLabel(e.target.value);
+      setIsReadOnly(true);
+    }
+  }
+
+  useEffect(() => {
+    const input = inputRef.current;
+
+    if (isReadOnly === false) {
+      input.focus();
+    }
+  }, [isReadOnly]);
 
   return (
     <li className="notes_card">
       <div className="notes_card_header">
-        <input type="text" readOnly={true} value={header} />
+        <div className="notes_card_label">
+          <input
+            type="text"
+            readOnly={isReadOnly}
+            placeholder="enter a label"
+            onChange={changeHandler}
+            value={label}
+            onKeyUp={eventHandler}
+            ref={inputRef}
+          />
+          <FiEdit2 onClick={editLabel} />
+        </div>
         <div className="notes_card_btns">
           <button>1</button>
           <button>2</button>
