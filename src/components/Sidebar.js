e1e368dbc15e082a5670ee07148b3f2807064d5c
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2024-06-03 10:17:21 -0400

    minor tweaks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,61 +12,37 @@ const Sidebar = ({ clickHandler }) => {
     <div className="sidebar">
       <div className="sidebar_header">
         <FiX onClick={clickHandler} className="sidebar-close" />
-        <Link to="/" style={styles.link} onClick={clickHandler}>
+        <Link to="/" onClick={clickHandler}>
           <h3>Trade Help</h3>
         </Link>
       </div>
 
       <ul className="sidebar_links">
         <li>
-          <Link
-            className="sidebar_link"
-            to="/avg"
-            style={styles.link}
-            onClick={clickHandler}
-          >
-            <span className="sidebar_icon">
-              <PiCalculatorThin />
-            </span>
-            <span> Stock Average Calc</span>
+          <Link className="sidebar_link" to="/avg" onClick={clickHandler}>
+            <PiCalculatorThin className="sidebar_icon" />
+            Stock Average Calc
           </Link>
         </li>
         <li>
           <Link
             className="sidebar_link"
             to="/private-notes"
-            style={styles.link}
             onClick={clickHandler}
           >
-            <span className="sidebar_icon">
-              <PiNotepad />
-            </span>
-            <span> Private Notes</span>
+            <PiNotepad className="sidebar_icon" />
+            Private Notes
           </Link>
         </li>
         <li>
-          <Link
-            className="sidebar_link"
-            to="/calendar"
-            style={styles.link}
-            onClick={clickHandler}
-          >
-            <span className="sidebar_icon">
-              <PiCalendarDotLight />
-            </span>
-            <span> Trading Calendar</span>
+          <Link className="sidebar_link" to="/calendar" onClick={clickHandler}>
+            <PiCalendarDotLight className="sidebar_icon" />
+            Trading Calendar
           </Link>
         </li>
       </ul>
     </div>
   );
-};
-
-const styles = {
-  link: {
-    textDecoration: "none",
-    color: "#494949",
-  },
 };
 
 export default Sidebar;
